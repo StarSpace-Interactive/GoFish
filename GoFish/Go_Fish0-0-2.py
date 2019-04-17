e1a1339d94bdb3f97ef1b846843d2783f3c82d9f
nova4026 <41805511+nova4026@users.noreply.github.com>
2019-04-17 16:34:04 -0700

    this is version 0-0-3
    In this version, I finished writing out all the code for the basic text-based engine 
    now the game is playable.though it not ready fo release

--- GoFish/Go_Fish0-0-2.py
+++ GoFish/Go_Fish0-0-2.py
@@ -1,36 +1,43 @@
 import random
+from collections import Counter
 
-Scard = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+pscore = 0#player score
+cscore = 0#computers score
+
+Scard = [1,2,3,4,5,6,7,8,9,10,11,12,13]#the cards
 Suites = 4
-deck = Suites * Scard
+deck = Suites * Scard#the card deck
 
-Cplayer_hand = [] #player number = 2
-Pplayer_hand = [] #player number = 1
+
+computer_hand = [] #player number = 2
+player_hand = [] #player number = 1
 
 
 
 def deal(player,NumOfcards):
     
-    '''deals out cards to the players'''
+    '''deals out cards to the players,'''
     if player == 1:
         for item in range(0,NumOfcards):
             card=random.choice(deck)
-            Pplayer_hand.append(card)
+            player_hand.append(card)
             deck.remove(card)
+            
     elif player == 2:
         for item in range(0,NumOfcards):
             Card = random.choice(deck)
-            Cplayer_hand.append(Card)
+            computer_hand.append(Card)
             deck.remove(Card)
             
 def player_guess():
-    print("your cards are: ",Pplayer_hand)
+    print("your cards are: ",player_hand)
+    print(f'you have this many matches: {pscore}')
     result = int(input("what card would you like to guess? "))
-    if result in Pplayer_hand: # may not be needed for GUI version)
-        if result in Cplayer_hand:
+    if result in player_hand: # may not be needed for GUI version)
+        if result in computer_hand:
             print("the computer gives this card ",result)
-            Cplayer_hand.remove(result)
-            Pplayer_hand.append(result)
+            computer_hand.remove(result)
+            player_hand.append(result)
         else:
             print("the computer does not have this card , go fish ")
             deal(1,1)
@@ -39,18 +46,18 @@ def player_guess():
         print("I'm sorry you don't have that card ")
 
 def Computer_guess():
-    result = random.choice(Cplayer_hand)
+    result = random.choice(computer_hand)
     print("the computer guesses:", result)
-    if result in Pplayer_hand:
+    print(f'the computer has this many matches: {cscore}')
+    if result in player_hand:
         print("you give this card: ", result)
-        Pplayer_hand.remove(result)
-        Cplayer_hand.append(result)
+        player_hand.remove(result)
+        computer_hand.append(result)
     else:
         print("the computer fishes")
-        deal(1,1)
+        deal(2,1)
 
-#check to see if there are 4 matching numbers in the players\computers hand
-#increse the players\computer match counter by however many matches they made during that turn
+
 #game loop
 
 deal(1,7)
@@ -60,10 +67,21 @@ Running = True
 while Running:
     print("")
     print("this is the deck :", deck)
-    print(len(deck))
-    print(" ")
-    print("computers cards", Cplayer_hand)
-    player_guess()
-    print(" ")
-    print("the computers turn.")
-    Computer_guess()
+    print("computers cards", computer_hand)
+    player_guess()#allows the player to guess a card
+    print()
+    Computer_guess()#allows the computer to guess a card
+    
+    pcounts = Counter(player_hand)#logic for checking if the players has a match
+    for num, count in pcounts.items():
+        if count >= 4:
+            player_hand = list(filter((num).__ne__, player_hand))
+            pscore += 1
+    
+    ccounts = Counter(computer_hand)#logic for checking if the computer has a match
+    for num, count in ccounts.items():
+        if count >= 4:
+            computer_hand = list(filter((num).__ne__, computer_hand))
+            cscore += 1
+    
+            
